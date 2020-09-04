0211837131b996b5d210afb6e0683c27a466c0ae
LukaszLeszczynski <lukaszleszczynski2@gmail.com>
2020-09-05 00:08:02 +0100

    [Initiate the project]

---
+++ src/main/java/com/example/cryptocurrencywallet/CryptocurrencywalletApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptocurrencywallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrencywalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (CryptocurrencywalletApplication.class, args);
+    }
+
+}
