5ad2be20bbcf80e9d7661c16196602a91897b8e4
LukaszLeszczynski <lukaszleszczynski2@gmail.com>
2020-09-05 12:01:45 +0100

    [Service]

--- src/main/java/com/example/cryptocurrencywallet/CryptocurrencywalletApplication.java
+++ src/main/java/com/example/cryptocurrencywallet/CryptocurrencywalletApplication.java
@@ -9,5 +9,5 @@ public class CryptocurrencywalletApplication {
     public static void main(String[] args) {
         SpringApplication.run (CryptocurrencywalletApplication.class, args);
     }
-
+sdasdasd
 }
