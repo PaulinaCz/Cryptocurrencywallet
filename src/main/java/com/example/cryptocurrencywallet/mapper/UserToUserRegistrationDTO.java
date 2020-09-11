dbf1d9631971c08e293d32aaef70fb3b7709a2a4
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-09-11 13:41:03 +0100

    added mapper for userDTO

---
+++ src/main/java/com/example/cryptocurrencywallet/mapper/UserToUserRegistrationDTO.java
@@ -0,0 +1,30 @@
+package com.example.cryptocurrencywallet.mapper;
+
+
+import com.example.cryptocurrencywallet.dto.UserRegistrationDTO;
+import com.example.cryptocurrencywallet.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserToUserRegistrationDTO {
+
+    public Optional<UserRegistrationDTO> registrationDTO(Optional<User> optionalUser){
+
+        if(optionalUser.isEmpty()){
+            return Optional.empty();
+        }else{
+            User user = optionalUser.get();
+            UserRegistrationDTO userRegistrationDTO = new UserRegistrationDTO();
+            userRegistrationDTO.setFirst_name(user.getFirstName());
+            userRegistrationDTO.setSurname(user.getSurname());
+            userRegistrationDTO.setEmail(user.getEmail());
+            userRegistrationDTO.setPassword(user.getPassword());
+
+            return Optional.of(userRegistrationDTO);
+        }
+
+    }
+
+}
