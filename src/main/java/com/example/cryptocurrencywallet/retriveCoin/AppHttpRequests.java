c4d5dee6fa819505852967b86f23b6ee6506d6a2
KonarzewskiP <pawelkonarzewski13@gmail.com>
2020-09-11 16:06:41 +0100

    Http request to get coins from API.

---
+++ src/main/java/com/example/cryptocurrencywallet/retriveCoin/AppHttpRequests.java
@@ -0,0 +1,52 @@
+package com.example.cryptocurrencywallet.retriveCoin;
+
+import com.example.cryptocurrencywallet.retriveCoin.model.CryptoCurrency;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.List;
+
+public class AppHttpRequests {
+    private static final String POSTS_API_URL = "https://api.nomics.com/v1/currencies/ticker?key=";
+    private static final String KEY = "0e3cdb66fd5f50dd96f21ece8c02468d";
+
+    static List<CryptoCurrency> requestCoin(String name) throws IOException, InterruptedException {
+        var client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .header("accept", "application/json")
+                .uri(URI.create(String.format(POSTS_API_URL + KEY + "&ids=%s",name)))
+                .timeout(Duration.ofSeconds(10)) // HttpTimeoutException
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // parse JSON into object
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        List<CryptoCurrency> temp = gson.fromJson(response.body(), new TypeToken<List<CryptoCurrency>>(){}.getType());
+
+        temp.forEach(System.out::println);
+
+        return temp;
+    }
+
+
+    public static void main(String[] args) {
+
+        try{
+            List<CryptoCurrency> coin = requestCoin("BTC,ETH,XRP");
+            coin.forEach(System.out::println);
+        } catch (Exception exc){
+            exc.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/com/example/cryptocurrencywallet/retriveCoin/model/CryptoCurrency.java
@@ -0,0 +1,31 @@
+package com.example.cryptocurrencywallet.retriveCoin.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class CryptoCurrency {
+
+private String id;
+private String currency;
+private String symbol;
+private String name;
+@SerializedName("logo_url")
+private String logoUrl;
+@JsonFormat(shape = JsonFormat.Shape.STRING)
+private BigDecimal price;
+private String rank;
+@SerializedName("max_supply")
+private String maxSupply;
+@SerializedName("circulating_supply")
+private String circulatingSupply;
+
+}
