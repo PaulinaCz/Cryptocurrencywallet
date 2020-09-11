dd6e5bef2ddf79edb94e6a272669ccef1da4b3ac
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-09-11 13:40:25 +0100

    fixed repositories

--- src/main/java/com/example/cryptocurrencywallet/repository/WalletRepository.java
+++ src/main/java/com/example/cryptocurrencywallet/repository/WalletRepository.java
@@ -2,10 +2,10 @@ package com.example.cryptocurrencywallet.repository;
 
 import com.example.cryptocurrencywallet.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
-    Optional<Wallet> findByUserId(Long userId);
 }
