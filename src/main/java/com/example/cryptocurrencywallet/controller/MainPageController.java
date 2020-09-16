1d3d3ae0fe16c6049814f76011f3830fd3744a75
Adrian Los <losadrian@gmail.com>
2020-09-16 21:03:09 +0100

    [Transaction Finished] no DATA connection YET

---
+++ src/main/java/com/example/cryptocurrencywallet/controller/MainPageController.java
@@ -0,0 +1,4 @@
+package com.example.cryptocurrencywallet.controller;
+
+public class MainPageController {
+}
--- src/main/java/com/example/cryptocurrencywallet/transactions/Transaction.java
+++ src/main/java/com/example/cryptocurrencywallet/transactions/Transaction.java
@@ -18,7 +18,7 @@ public class Transaction {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "transaction_id") //no data for transaction_id YET!
+    @Column(name = "transaction_id") //no data connection for transaction_id YET!
 
     private long id;
 
