7c8516486f6f563cdce0d76261d1a9d6c3386d96
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-09-24 21:49:00 +0100

    adjust accountDetails branch to updated master

---
+++ src/main/java/com/example/cryptocurrencywallet/controller/AccountDetailsController.java
@@ -0,0 +1,32 @@
+package com.example.cryptocurrencywallet.controller;
+
+import com.example.cryptocurrencywallet.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@PreAuthorize("hasRole('ROLE_USER')")
+public class AccountDetailsController {
+
+    @Autowired
+    UserService userService;
+
+    @ModelAttribute
+    public void saveLoggedUserDetails(Model model) {
+        model.addAttribute("loggedUser", userService.getLoggedUser());
+        model.addAttribute("loggedUserBalance", userService.getLoggedUser().getWallet().getBalanceUSD());
+        model.addAttribute("loggedUserCurrencies", userService.getLoggedUser().getWallet().getMyCoins());
+        model.addAttribute("loggedUserTransactionHistory", userService.getLoggedUser().getWallet().getTransactionHistories());
+
+    }
+
+    @GetMapping(value = "/accountDetails")
+    public String getAccountDetailsPage() {
+        return "accountDetails";
+    }
+
+}
