5468eccac3536bcf8f3b4a158344d707a24ce023
PaulinaCz <paulinaczernieckaa@gmail.com>
2020-10-13 19:21:26 +0100

    AccountDetailsController moved from v1

--- src/main/java/com/example/cryptocurrencywallet/controller/AccountDetailsController.java
+++ src/main/java/com/example/cryptocurrencywallet/controller/AccountDetailsController.java
@@ -1,26 +1,38 @@
 package com.example.cryptocurrencywallet.controller;
 
+import com.example.cryptocurrencywallet.retriveCoin.model.CryptoCurrency;
+import com.example.cryptocurrencywallet.service.CryptoCoinDetails;
 import com.example.cryptocurrencywallet.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @Controller
 @PreAuthorize("hasRole('ROLE_USER')")
 public class AccountDetailsController {
 
-    @Autowired
+
     private UserService userService;
+    private CryptoCoinDetails coinDetails;
+
+    public AccountDetailsController(UserService userService, CryptoCoinDetails coinDetails) {
+        this.userService = userService;
+        this.coinDetails = coinDetails;
+    }
 
     @ModelAttribute
     public void saveLoggedUserDetails(Model model) {
+        List<CryptoCurrency> listOfCrypto = coinDetails.getListOfCryptoCurrencies("BTC,ETH,USDT,XRP,BCH,DOT,LINK,BNB,CRO,LTC");
+
         model.addAttribute("user", userService.getLoggedUser());
         model.addAttribute("loggedUserBalance", userService.getLoggedUser().getWallet().getBalanceUSD());
         model.addAttribute("loggedUserCurrencies", userService.getLoggedUser().getWallet().getMyCoins());
         model.addAttribute("loggedUserTransactionHistory", userService.getLoggedUser().getWallet().getTransactionHistories());
+        model.addAttribute("cryptoCoinDetails", listOfCrypto);
 
     }
 
